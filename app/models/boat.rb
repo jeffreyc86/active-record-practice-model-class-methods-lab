4fb652f7d87a6d8309bd23fbd1edcf67bda5dd0c
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2020-11-30 13:32:10 -0500

    commit tree 4ac36e93cd47fccec1676da2157ce7deaf8cc2bc

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -4,27 +4,28 @@ class Boat < ActiveRecord::Base
   has_many    :classifications, through: :boat_classifications
 
   def self.first_five
-    # all.limit(5)
+    self.limit(5).to_a
   end
 
   def self.dinghy
-    # where("length < 20")
+    self.where('length < 20')
   end
 
   def self.ship
-    # where("length >= 20")
+    self.where('length >= 20')
   end
 
   def self.last_three_alphabetically
-    # all.order(name: :desc).limit(3)
+    self.limit(3).order('name desc')
   end
 
   def self.without_a_captain
-    # where(captain_id: nil)
+    self.where(captain_id: nil)
   end
 
   def self.sailboats
     # includes(:classifications).where(classifications: { name: 'Sailboat' })
+    self.where('classifications.name = Sailboat')
   end
 
   def self.with_three_classifications
